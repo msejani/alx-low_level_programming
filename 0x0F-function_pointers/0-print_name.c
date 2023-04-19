51baffb6b9411e53a160e5753d2159fb0a711d15
msejani <sejanimaambo@gmail.com>
2023-04-19 11:45:13 -0700

    Task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include "function_pointers.h"
-
-/**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
- */
-
-void print_name(char *name, void (*f)(char *));
-{
-	putchar("my name: %s\n", name);
-	return (0);
-}
+        #include <stdio.h>
+        /**
+         * print_name - print name using pointer to function
+         * @name: string to add
+         * @f: pointer to function
+         * Return: nothing
+         **/
+        void print_name(char *name, void (*f)(char *))
+        {
+                if (name == NULL || f == NULL)
+                        return;
+                f(name);
+        }
