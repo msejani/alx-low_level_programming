fe29f5c30a993847266b19df9b2a5612ac121be7
msejani <sejanimaambo@gmail.com>
2023-04-19 11:27:38 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *));
+{
+	putchar("my name: %s\n", name);
+	return (0);
+}
