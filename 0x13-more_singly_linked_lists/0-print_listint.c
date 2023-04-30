6954362917ccd3c27a9c5e4acf199c3d67dae40d
msejani <sejanimaambo@gmail.com>
2023-04-30 10:10:17 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ * 
+ * Return: Always 0.
+ */
+size_t print_listint(const listint_t *h);
+
+int main(void)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+          printf("%d--->\n" , h->next;
+	return(num);
+	}
